1a0dd6a3dcb30ee282f4508ae82932790652bb69
durrlov <bakhtiar.scr@gmail.com>
2025-08-30 14:34:26 +0600

    setup & requirements

---
+++ setup.py
@@ -0,0 +1,28 @@
+from setuptools import find_packages, setup
+from typing import List
+
+
+HYPEN_E_DOT = '-e .'
+def get_requirements(file_path:str) -> List[str]:
+    '''
+    this function will return the list of requirements
+    '''
+    requirements = []
+    with open(file_path, encoding='utf-8', errors='ignore') as file_obj:
+        requirements = file_obj.readlines()
+        requirements = [req.replace("\n", "") for req in requirements]
+
+        if HYPEN_E_DOT in requirements:
+            requirements.remove(HYPEN_E_DOT)
+
+    return requirements
+
+
+setup(
+name = 'booster_landing_ml_prediction',
+version = '0.0.1',
+author = 'Bakhtiar',
+author_email = 'bakhtiar.scr@gmail.com',
+packages = find_packages(),
+install_requires = get_requirements('requirements.txt')
+)
