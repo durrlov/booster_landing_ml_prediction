1b05995bcd87488b9cecc8db64ef250cd823ca9c
durrlov <bakhtiar.scr@gmail.com>
2025-09-18 00:36:15 +0600

    data ingestion

--- src/components/data_ingestion.py
+++ src/components/data_ingestion.py
@@ -23,6 +23,28 @@ class DataIngestion:
 
         try:
             df = pd.read_csv("notebook/data/data_from_api.csv")
+            logging.info('Dataset has been read as a dataframe')
+
+            os.makedirs(os.path.dirname(self.data_ingestion_config.raw_data_path), exist_ok= True)
+            df.to_csv(self.data_ingestion_config.raw_data_path, index= False)
+            logging.info('Raw data has been saved to artifacts/data.csv')
+
+            train_set, test_set = train_test_split(df, test_size= 0.2, random_state= 42)
+            
+            train_set.to_csv(self.data_ingestion_config.train_data_path, index= False)
+            test_set.to_csv(self.data_ingestion_config.test_data_path, index= False)
+            logging.info("Train and test data has been saved to artifacts")
+
+            return(
+                self.data_ingestion_config.train_data_path,
+                self.data_ingestion_config.test_data_path
+            )
         
-        except:
-            pass
+        except Exception as e:
+            raise CustomException(e, sys)
+        
+
+if __name__ == "__main__":
+    obj = DataIngestion()
+    train_path, test_path = obj.initiate_data_ingestion()
+    logging.info(f"Data ingestion complete. Train data at: {train_path}, Test data at: {test_path}")
